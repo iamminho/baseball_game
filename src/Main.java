62f44107862dfc96f7526c3a57fb4925ca033dd3
iamminho <minho100227@kakao.com>
2022-10-11 11:59:23 +0900

    func inputCheck revise overlap number check

--- src/Main.java
+++ src/Main.java
@@ -27,9 +27,19 @@ public class Main {
 
     static boolean inputCheck (String input) {
         String pattern = "^[1-9]*$";
+        HashSet<Character> set = new HashSet<>();
 
+        //false 조건들
+
+        // 글자 수가 3개가 아닐때
         if (input.length() != 3) return false;
+
+        // 글자안에 특수문자, 공백, 0등 1~9의 숫자가 아닐때
         if (!Pattern.matches(pattern, input)) return false;
+
+        // 겹치는 숫자가 있을때
+        for (int i = 0; i < input.length(); i++) set.add(input.charAt(i));
+        if (set.size() != input.length()) return false;
 
         return true;
     }
