83224be06b72c51008af908d6d6451e9d72859b2
iamminho <minho100227@kakao.com>
2022-10-11 10:37:23 +0900

    function randomNum

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.*;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayList<Integer> randomNumber = randomNum();
     }
+
+    static ArrayList<Integer> randomNum() {
+        Random rd = new Random();
+        ArrayList<Integer> numberList =  new ArrayList<>();
+        int cnt = 0;
+
+        while(cnt < 3) {
+            int number = rd.nextInt(9) + 1;
+            if (!numberList.contains(number)) {
+                numberList.add(number);
+                cnt++;
+            }
+        }
+
+        return numberList;
+    }
+
+
 }
